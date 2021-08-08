0cacf488e299ab2a944a869a7bc3a8d677b085cc
Joe Perry <japerry16633@gmail.com>
2021-08-08 16:25:58 -0500

    calculator-hw

--- main.js
+++ main.js
@@ -30,12 +30,16 @@ const subtract = (numA, numB) => {
 // These variables are already defined but that don't point to functions. It's up to you to build the functions to complete your calculator use:
 
 const multiply = (numA, numB) => {
-  // * to get a product then return it
-  // Open up the inspector tool in Chrome and select the Console tab to see what this functions is "logging out" to the console.
-  console.log(numA, numB)
+    const product = numA * numB 
+    return product
+  
 }
 
-const divide = null
+const divide = (numA, numB) => {
+  const quotient = numA / numB 
+  return quotient
+
+}
 // / to get a quotient,
 
 const modulus = null
@@ -64,13 +68,12 @@ const equals = () => {
     break;
     case "subtraction": putResultInElement(subtract(firstNum, secondNum)) 
     break;
-    case "multiplication": multiply(firstNum, secondNum) 
+    case "multiplication": putResultInElement(multiply(firstNum, secondNum)) 
     break;
-    case "division": console.log(divide(firstNum, secondNum)) 
+    case "division": putResultInElement(divide(firstNum, secondNum)) 
     break;
     case "modulus": console.log(modulus(firstNum, secondNum)) 
     break;
     default: "Choose an operation"
   }
 }
-
